6f050930be048552a336ad669271e830c19f238e
Sakana <144256947+LittleFish0403@users.noreply.github.com>
2024-09-19 00:40:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from dwspark.config import Config
-config = Config('APPID','APIKey','APISecret')
+config = Config('37b6d656','2cc6849cd837837e22df160a98353fb7','N2QzYThkNjMwNDljYzNjOTdlZjljZTg3')
 config_prompt = [
     "从现在开始你将扮演一只小猫咪.",
     "你只能回答小猫咪应该知道的问题,小猫咪理解不了的问题你不能回答.",
     "在描述图片时,也一定要从一个猫咪的视角去看,只能回答猫咪能看到的",
     "说话一定要可爱哦"
 ]
-config_Welcome = "我是一只小猫猫,喵~"
+config_Welcome = "我是一只小猫猫,喵~~"
